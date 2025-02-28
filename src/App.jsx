72c33372f79cafdc18a2bf0696025a2e10861d1b
how <how@xd>
2025-02-28 19:41:41 +0100

    feat: new dogs page

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import { Routes, Route } from 'react-router';
 import { Home } from './routes/Home';
 import { Navbar } from './components/Navbar';
+import { Dogs } from './routes/Dogs';
 
 const App = () => {
   return (
     <Routes>
       <Route element={<Navbar />}>
-        <Route path="/" element={<Home />} />
+      <Route path="/" element={<Home />} />
+      <Route path="/dogs" element={<Dogs />} />
       </Route>
     </Routes>
   );
--- src/components/Navbar/index.jsx
+++ src/components/Navbar/index.jsx
@@ -3,9 +3,12 @@ import { NavLink, Outlet } from 'react-router';
 export const Navbar = () => {
   return (
     <>
-      <nav className='bg-gray-800 text-white p-4'>
-        <NavLink to="/" end>
+      <nav className='flex bg-gray-800 text-white p-4 gap-2'>
+      <NavLink to="/" end>
           Home
+        </NavLink>
+        <NavLink to="/dogs" end>
+          Dogs
         </NavLink>
       </nav>
       <Outlet />
