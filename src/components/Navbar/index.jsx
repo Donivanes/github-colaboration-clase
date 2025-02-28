cbe1274444c1c4b7e982bdd57863794a6c272b91
Ivan <donivanes@gmail.com>
2025-02-28 19:46:30 +0100

    style: navbar margin

--- src/components/Navbar/index.jsx
+++ src/components/Navbar/index.jsx
@@ -3,11 +3,11 @@ import { NavLink, Outlet } from 'react-router';
 export const Navbar = () => {
   return (
     <>
-      <nav className='bg-gray-800 text-white p-4'>
-        <NavLink to="/" end>
+      <nav className="bg-gray-800 text-white p-4">
+        <NavLink to="/" end className="mr-4">
           Home
         </NavLink>
-        <NavLink to="/about" end>
+        <NavLink to="/about" end className="mr-4">
           About
         </NavLink>
       </nav>
